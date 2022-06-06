2d317dbfe3aaf4d93fb54dea61e36d876282fd4e
Иван Шульженко <ilitetop@yandex.ru>
2022-06-06 21:02:40 +0700

    Контекст

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,10 +1,18 @@
 import { ReactElement } from 'react';
+import React, { useContext } from 'react';
 
 type Theme = 'light' | 'dark';
+let ThemeContext = React.createContext<Theme>('light');
 
-export function ThemeProvider(props: {
-    theme: Theme;
-    children: ReactElement;
-}) {}
+export function ThemeProvider(props: { theme: Theme; children: ReactElement }) {
+    return (
+        <ThemeContext.Provider value={props.theme}>
+            <div>{useTheme()}</div>
+        </ThemeContext.Provider>
+    );
+}
 
-export function useTheme(): Theme {}
+export function useTheme(): Theme {
+    const theme = useContext(ThemeContext);
+    return theme;
+}
